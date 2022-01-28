63071870d252d71a81b2f02e3e291e2e669a5546
MarosiAlexandra <97289704+MarosiAlexandra@users.noreply.github.com>
2022-01-28 13:18:24 +0100

    2/3 feladat

---
+++ 2022-01-28_Opening-weekend/Film.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class Film
+    {
+        public string eredetiCim { get; set; }
+        public string magyarCim { get; set; }
+        public string bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int bevétel { get; set; }
+        public int látogatók { get; set; }
+
+        public Film(string sor)
+        {
+            string[] t = sor.Split(';');
+            eredetiCim = t[0];
+            magyarCim = t[1];
+            bemutato = t[2];
+            forgalmazo = t[3];
+            bevétel = int.Parse(t[4]);
+            látogatók= int.Parse(t[5]);
+
+
+        }
+    }
+}
